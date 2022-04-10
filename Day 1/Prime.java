78572d5788c45ff5636515b3994865da2e992686
Priyanka <priyankap1439@gmail.com>
2022-04-11 00:27:02 +0530

    Day 1

--- Day 1/Prime.java
+++ Day 1/Prime.java
@@ -1,3 +1,9 @@
+/* Write a Java program to check if a given number is prime or not. Remember, 
+a prime number is a number which is not divisible by any other number, e.g. 3, 5, 7, 11, 13, 17, etc. 
+Be prepared for cross, e.g. checking till the square root of a number, etc.
+
+Read more: https://javarevisited.blogspot.com/2017/07/top-50-java-programs-from-coding-Interviews.html#ixzz7Q5EsfS9y */
+
 import java.util.*;
 
 public class Prime {
--- Day 1/Reverse.java
+++ Day 1/Reverse.java
@@ -1,3 +1,11 @@
+/* You need to write a simple Java program to check if a given String is palindrome or not.
+A Palindrome is a String which is equal to the reverse of itself, e.g.,
+"Bob" is a palindrome because of the reverse of "Bob" is also "Bob." 
+Though be prepared with both recursive and iterative solution of this problem.
+The interviewer may ask you to solve without using any library method, e.g. indexOf() or subString() so be prepared for that.
+
+Read more: https://javarevisited.blogspot.com/2017/07/top-50-java-programs-from-coding-Interviews.html#ixzz7Q5F2HTvH */
+
 import java.util.*;
 
 public class Reverse {
